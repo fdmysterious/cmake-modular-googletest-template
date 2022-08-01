3eac7b0ea6d5eb11d8a66a3364eb1d352c3f7f70
Florian Dupeyron <florian.dupeyron@mugcat.fr>
2022-08-01 14:40:05 +0200

    Add clang-tidy to component testing mode

---
+++ src/config/ClangTidyConfig.cmake
@@ -0,0 +1,29 @@
+set(CLANG_TIDY_CHECKS_LIST
+    "cppcoreguidelines-*"                    # Enable core cpp guidelines checks
+    "bugprone-*"                             # Enable bugprone checks
+    "readability-*"                          # Enable readability checks
+    "hicpp-*"                                # Enable high integrity CPP checks
+)
+
+
+set(CLANG_TIDY_ERRORS_LIST
+    "*"                                      # All enabled checks can trigger an error
+    "-cppcoreguidelines-avoid-magic-numbers" # Disable magic number error
+    "-readability-magic-numbers"             # --
+)
+
+
+#####################################################################
+
+string(REPLACE ";" "," CLANG_TIDY_CHECKS "${CLANG_TIDY_CHECKS_LIST}")
+string(REPLACE ";" "," CLANG_TIDY_ERRORS "${CLANG_TIDY_ERRORS_LIST}")
+
+message(STATUS "Clang TIDY checks: ${CLANG_TIDY_CHECKS}")
+message(STATUS "Clang TIDY errors: ${CLANG_TIDY_ERRORS}")
+
+set(CMAKE_C_CLANG_TIDY
+    clang-tidy;
+    --header-filter=.;
+    --checks=${CLANG_TIDY_CHECKS};
+    --warnings-as-errors=${CLANG_TIDY_ERRORS};
+)
